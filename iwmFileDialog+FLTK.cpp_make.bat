c4f5d206b4cb4c41a54f767f4b66581ff983ab50
iwm-iwama <52396966+iwm-iwama@users.noreply.github.com>
2024-12-17 11:16:24 +0900

    Add files via upload

--- iwmFileDialog+FLTK.cpp_make.bat
+++ iwmFileDialog+FLTK.cpp_make.bat
@@ -9,13 +9,14 @@ for /f "delims=. tokens=1,2" %%i in ("%src%") do (
 	set fn=%%i%
 )
 set fn_exe=%fn%.exe
-set cc=g++.exe -std=c++23
+set cc=g++.exe
+:: -std=c++23
 set lib=lib_iwmutil2.a
-:: -static 以降は bash $(fltk-config --ldstaticflags) を展開したもの
+:: -static 以降は `fltk-config --ldstaticflags` を展開したもの
 :: -Wl,-subsystem,console => With Console
 :: -Wl,-subsystem,windows => Without Console
 set subsystem=console
-set op_link=-Os -Wall -Wextra -lgdi32 -luser32 -lshlwapi -static -pipe -Wl,-subsystem,%subsystem% -mwindows c:\msys64\mingw64\lib\libfltk.a -lole32 -luuid -lcomctl32 -lws2_32
+set op_link=-Os -Wall -Wextra -lgdi32 -luser32 -lshlwapi -static -pipe -Wl,-subsystem,%subsystem% -mwindows -lfltk -lgdiplus -lole32 -luuid -lcomctl32 -lws2_32 -lwinspool
 
 :: Make
 	echo --- Make ------------------------------------------
